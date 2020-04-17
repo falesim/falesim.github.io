1f1291fe3fc4119f42a24cfba19e46381b93368e
sspantz <sspantiezheng@gmail.com>
2020-04-17 18:54:52 +0800

    update index

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: "falesim", // Title for your website.
-  tagline: "A company with creative",
+  tagline: "A team with creative",
   url: "https://falesim.com", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
