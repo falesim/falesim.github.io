3ec76fbc8e6b1445e79207f7bc255ad05e5d84b1
sspantz <sspantiezheng@gmail.com>
2020-04-17 16:03:49 +0800

    publish the first post

---
+++ website/blog/2020-04-17-first-blog.md
@@ -0,0 +1,10 @@
+---
+title: First Blog
+author: sspantz
+authorURL: https://github.com/sspantz
+---
+This is the first version of falesim home page.
+<!--truncate-->
+Everything is not ready for publishing.
+This post is just for test only.
+This website will be published soon or later.
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,51 +11,54 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: "User1",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
+    image: "/img/undraw_open_source.svg",
+    infoLink: "https://www.facebook.com",
+    pinned: true
+  }
+]
 
 const siteConfig = {
-  title: 'falesim', // Title for your website.
-  tagline: 'A company with creative',
-  url: 'https://falesim.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: "falesim", // Title for your website.
+  tagline: "A company with creative",
+  url: "https://falesim.com", // Your website URL
+  baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'falesim',
-  organizationName: 'falesim',
+  projectName: "falesim",
+  organizationName: "falesim",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: "doc1", label: "Docs"},
+    {doc: "doc4", label: "API"},
+    {page: "help", label: "Help"},
+    {blog: true, label: "Blog"}
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+  headerIcon: "img/favicon.ico",
+  footerIcon: "img/favicon.ico",
+  favicon: "img/favicon.ico",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#8e378a',
-    secondaryColor: '#632660',
+    primaryColor: "#3198e8",
+    secondaryColor: "#277ABA",
+    thirdColor: "#1D5C8C",
+    fourthColor: "#143D5D",
+    fifthColor:"#0A1F2F"
   },
 
   /* Custom fonts for website */
@@ -77,20 +80,20 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ["https://buttons.github.io/buttons.js"],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg"
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
@@ -105,6 +108,6 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
   //   repoUrl: 'https://github.com/facebook/test-site',
-};
+}
 
-module.exports = siteConfig;
+module.exports = siteConfig
